1035918496de5ae642d6ad8c8e45b4f59548efaa
Eyad Mohammed <108591924+Jackgawe@users.noreply.github.com>
2026-05-06 21:13:00 +0300

    feature: SEO

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,svg,png,woff2}"],
+        additionalManifestEntries: [{ url: "/og-image.png", revision: null }],
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
